aaf83405f717d77d165e92bdc634effe65627c8b
januarytw <januarytw@126.com>
2018-06-29 18:00:47 +0800

    1

--- class_0626/作业_0626.py
+++ class_0626/作业_0626.py
@@ -10,7 +10,7 @@
 6：附加作业：请利用思维导图工具xmind画出你写代码的思路以及调用关系（要写!!!)
 '''
 
-# 操作excel
+# 操作excel类
 from openpyxl import load_workbook
 class DoExcel():
     def __init__(self,file_path,sheet_name):
@@ -47,12 +47,13 @@ class DoExcel():
         except Exception as e:
             print("写入出错了：",e)
 
-#请求接口
+#请求接口类
 import requests
 
 doExcel=DoExcel('case.xlsx','test_data')
 excel_result=doExcel.readData()
 
+#如果需要登录操作时，调用此session
 url='http://119.23.241.154:8080/futureloan/mvc/api/member/login'
 data={'mobilephone':'13141222222','pwd':'123456'}
 s=requests.session()
@@ -60,24 +61,35 @@ s.post(url,data)
 
 for item in excel_result:
     if item[1]=="注册":
-        #注册
-        login_url=item[4]
-        login_data=eval(item[5])
-        result=requests.get(login_url,login_data).json()
-        doExcel.writeData(item[0]+1,8,str(result))
+        try:
+            #注册
+            login_url=item[4]
+            login_data=eval(item[5])
+            result=requests.get(login_url,login_data).json()
+            doExcel.writeData(item[0]+1,8,str(result))
+        except Exception as e:
+            print ("请求失败，出现的错误是%s"%e)
+            raise e
     elif item[1]=="登录":
-        #登录
-        login_url=item[4]
-        login_data=eval(item[5])
-        print(login_data)
-        result_login=requests.post(login_url,login_data).json()
-        # print(result_login)
-        doExcel.writeData(item[0]+1,8,str(result_login))
-
+        try:
+            login_url=item[4]
+            login_data=eval(item[5])
+            print(login_data)
+            result_login=requests.post(login_url,login_data).json()
+            # print(result_login)
+            doExcel.writeData(item[0]+1,8,str(result_login))
+        except Exception as e:
+            print ("请求失败，出现的错误是%s"%e)
+            raise e
     elif item[1]=="充值":
-        #充值
-        login_url=item[4]
-        login_data=eval(item[5])
-        result_recharge=s.post(login_url,login_data).json()
-        # print(result_recharge)
-        doExcel.writeData(item[0]+1,8,str(result_recharge))
+        try:
+            #充值
+            login_url=item[4]
+            login_data=eval(item[5])
+            result_recharge=s.post(login_url,login_data).json()
+            # print(result_recharge)
+            doExcel.writeData(item[0]+1,8,str(result_recharge))
+        except Exception as e:
+            print ("请求失败，出现的错误是%s"%e)
+            raise e
+
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-import mysql.connector
+import requests
+
+url='http://119.23.241.154:8080/futureloan/mvc/api/member/login'
+data={'mobilephone':'13141222222','pwd':'123456'}
+s=requests.session()
+s.get(url,data)

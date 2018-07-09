049088e194a9478feb86bcd529b42a8fae95fe3c
januarytw <januarytw@126.com>
2018-07-10 07:32:19 +0800

    1

--- auto_infterface_case/common/my_log.py
+++ auto_infterface_case/common/my_log.py
@@ -37,7 +37,7 @@ class MyLog:
 
         return logger'''
 
-    def myLog(self,msg,msg_level,log_name='auto_cases',level='DEBUG',formatStr,file_path=project_path.log_path):
+    def myLog(self,msg,msg_level,log_name='auto_cases',level='DEBUG',file_path=project_path.log_path):
         #创建日志收集器,设置级别
         logger=logging.getLogger(log_name)
         logger.setLevel(level)
@@ -49,4 +49,41 @@ class MyLog:
         sh.setLevel(level)
 
         #设置输出格式
-        formatter=logging.Formatter(formatStr)
+        formatter=logging.Formatter('[%(levelname)s]%(asctime)s[日志信息]:%(message)s')
+        fh.setFormatter(formatter)
+        sh.setFormatter(formatter)
+
+        logger.addHandler(sh)
+        logger.addHandler(fh)
+        if msg_level=='DEBUG':
+            logger.debug(msg)
+        elif msg_level=='INFO':
+            logger.info(msg)
+        elif msg_level=='WARNING':
+            logger.warning(msg)
+        elif msg_level=='ERROR':
+            logger.error(msg)
+        elif msg_level=='CRITICAL':
+            logger.critical(msg)
+        #使用完成后要记得移除handler
+        logger.removeHandler(fh)
+        logger.removeHandler(sh)
+
+    def debug(self,msg):
+        self.myLog(msg,'DEBUG')
+
+    def info(self,msg):
+        self.myLog(msg,'INFO')
+
+    def warning(self,msg):
+        self.myLog(msg,'WARNING')
+
+    def error(self,msg):
+        self.myLog(msg,'ERROR')
+
+    def critical(self,msg):
+        self.myLog(msg,'CRITICAL')
+
+
+if __name__ == '__main__':
+    MyLog().info('hello')

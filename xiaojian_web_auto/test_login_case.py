e056978db2a009d758c3d7e038a3a725c6c8097d
januarytw <januarytw@126.com>
2018-08-06 00:48:48 +0800

    1

--- xiaojian_web_auto/test_login_case.py
+++ xiaojian_web_auto/test_login_case.py
@@ -1,22 +1,46 @@
 from selenium import webdriver
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
 import time
-
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-#打开网址
-driver.get("http://120.79.176.157:8012/Index/login.html")
 
-#登录
-driver.find_element_by_xpath('//input[@name="phone"]').send_keys("18684750553")
-driver.find_element_by_xpath('//input[@name="password"]').send_keys("python")
-driver.find_element_by_tag_name("button").click()
+def login(url, uid, pwd):
+    #打开网址
+    driver.get(url)
+
+    #登录
+    driver.find_element_by_xpath('//input[@name="phone"]').send_keys(uid)
+    driver.find_element_by_xpath('//input[@name="password"]').send_keys(pwd)
+    driver.find_element_by_tag_name("button").click()
+
+#正常登录
+login("http://120.79.176.157:8012/Index/login.html", "18684720553", "python")
 
 # 验证
 try:
+    usernameXpath='//a[@href="/Member/index.html" and text()="我的帐户[小蜜蜂96027]"]'
+    WebDriverWait(driver,20,1).until(EC.visibility_of_element_located(By.XPATH,usernameXpath))
+except:
+    print("没有找到用户昵称")
+
+time.sleep(2)
+driver.quit()
+
+#不输入密码登录
+login("http://120.79.176.157:8012/Index/login.html", "18684720553", "")
+
+#验证
+error_msg_xpath = "//div[@class='form-error-info']"
+check_msg = "请输入密码"
+WebDriverWait(driver, 20, 1).until(EC.visibility_of_element_located(By.XPATH, error_msg_xpath))
+
+if driver.find_element_by_xpath(error_msg_xpath).text==check_msg:
+    print("OK")
+else:
+    print("失败")
+time.sleep(2)
+driver.quit()
